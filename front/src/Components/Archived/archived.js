248ae253bcf6a23e7a7a048f888ea8b72c112632
nicolasab <nicolasab@epidataconsulting.com>
2022-04-28 22:27:49 -0300

    se subio el front

--- front/src/Components/Archived/archived.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Archived = () => {
-  return (
-    <div>archived</div>
-  )
-}
--- front/src/notesApp.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { AppRouters } from './routers/routers'
-
-export const NotesApp = () => {
-  return (
-   <AppRouters/>
-  )
-}
